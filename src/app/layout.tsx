f4224a42c491274fcb3bafe7310352247dcd2e39
Sai Vinay G <gsaivinay123@gmail.com>
2024-03-12 05:05:04 +0530

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,11 @@ import { Header } from "@/components/header";
 export const metadata = {
     metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
     title: {
-        default: "Next.js AI Chatbot",
-        template: `%s - Next.js AI Chatbot`,
+        default: "Claude 3 Prompt Optimizer",
+        template: `Claude 3 Prompt Optimizer`,
     },
     description:
-        "An AI-powered chatbot template built with Next.js and Vercel.",
+        "Claude 3 Prompt Optimizer",
     icons: {
         icon: "/favicon.ico",
         shortcut: "/favicon-16x16.png",
